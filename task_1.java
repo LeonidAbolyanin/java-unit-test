a6d88dffa8728bd5f639bcd54cb9e20e336603a6
Leonid Abolyanin <l.abolyanin@gmail.com>
2024-05-31 14:35:56 +0300

    My unit tests

---
+++ task_1.java
@@ -0,0 +1,6 @@
+@Test
+public void checkIsAdultWhenAgeIsMoreThan18True() {
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(19);
+	assertEquals("Ошибка проверки кода", true, isAdult);// Напиши код здесь
+}
